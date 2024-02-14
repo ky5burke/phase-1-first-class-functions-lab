8aa6ff8538b2ea7b5ce5c05fdecf699b3554009e
ky5burke <ky5burke@gmail.com>
2024-02-14 13:20:10 -0600

    handling first-class functions

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (list) {
+    return list.slice(0, 2)
+}
+
+const returnLastTwoDrivers = function (list) {
+    return list.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num) {
+    return function (price) {
+        return num * price;
+    };
+}
+
+function fareDoubler(fare) {
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, driverFunction) {
+    if (driverFunction === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    }
+    else if (driverFunction === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers);
+    }
+    else {
+        return "I can't do anything with this input."
+    }
+}
